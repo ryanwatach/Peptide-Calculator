c1d433a1ce9732c155d9a8b718cbc3dd8fd24b84
ryanwatach <ryanwatach@gmail.com>
2026-06-09 01:03:42 +0000

    added comments and updated functions

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 def main():
-    vial = vial_to_float(input("How many mg per vial? ")).lower()
-    bac = bac_to_float(input("How many mL of bacteriostatic water are you adding? ")).lower()
-    dose = dose_to_float(input("How many mg of the peptide do you want in each dose? ")).lower()
-    units_per_ml = vial / (bac)
-    correct_dose = dose/(units_per_ml) * 100
-    print(f"To have a dose of {dose:.2f} in a 1 mL syringe, draw {units_per_ml:.2f}")
+    # ask for mg/vial, bac water amount, and the dosage required
+    vial = vial_to_float(input("How many mg per vial? ").lower())
+    bac = bac_to_float(input("How many mL of bacteriostatic water are you adding? ").lower())
+    dose = dose_to_float(input("How many mg of the peptide do you want in each dose? ").lower())
+    
+    # mg of peptide per 1 mL of fluid
+    mg_per_ml = vial / (bac)
 
+    # standard U-100 insulin syringe conversion (100 units = 1 mL)
+    # (dose / concentration) * 100 units
+    correct_dose = dose/(mg_per_ml) * 100
+    print(f"To have a dose of {dose:.2f} in a 1 mL syringe, draw {correct_dose:.2f} units")
 
+# removes unwanted text
 def vial_to_float(v):
-    vnew = float(v.replace("mg" , "").replace(" dollars", ""))
+    vnew = float(v.replace("mg" , "").replace(" miligrams", ""))
     return vnew
 
 def bac_to_float(b):
@@ -16,7 +22,7 @@ def bac_to_float(b):
    return pnew
 
 def dose_to_float(d):
-   pnew = float(d.replace("%" , "").replace(" percent", ""))
+   pnew = float(d.replace("mg" , "").replace(" miligrams", ""))
    return pnew
 
 main()
